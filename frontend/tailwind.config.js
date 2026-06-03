53361be392375db82d68f71b1d0efc994fb6b57b
José Ignacio Candurin Herrera <josecandurin3@gmail.com>
2026-06-02 23:04:15 -0400

    Revert "Agregando libreria NextUI"

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,15 +1,8 @@
-import { nextui } from "@nextui-org/react";
-
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{ts,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}" // Esto es vital para que NextUI se vea
-  ],
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {}
   },
-  darkMode: "class",
-  plugins: [nextui()]
+  plugins: []
 }
