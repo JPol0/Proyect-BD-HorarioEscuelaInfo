c81e5dceb9bff6135485f7d94fa895b7d3d3ffd9
JPol0 <160655545+JPol0@users.noreply.github.com>
2026-06-27 23:21:14 -0400

    Feature new functions gestion materias (#9)
    * diseño Gestion Materias Terminado
    
    * feature: funcionalidad de Consultar informacion lista

---
+++ frontend/src/core/domain/services/MateriaServices.ts
@@ -0,0 +1,10 @@
+import { type Materia } from '../Materia'
+
+export const getByCodigo = (materias: Materia[], codMateria: string): Materia | undefined => {
+  return materias.find((materia) => materia.codMateria === codMateria)
+}
+
+export const calcularSemestreMaximo = (materias: Materia[]): number => {
+  if (!materias || materias.length === 0) return 0
+  return Math.max(...materias.map(m => m.semestre))
+}
