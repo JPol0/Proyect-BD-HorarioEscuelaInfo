e2ef7b9edcc8d66fe0945bb0e94ac1cfaa209a32
Pedro Luis Delgadillo Ferrer <pedrodelgadillo199@gmail.com>
2026-07-11 00:28:34 -0400

    Revert "Merge branch 'dev' into upload-plan-de-estudio"
    This reverts commit 959c3d1cecdbad500179fe66485f6c6f051776a4, reversing
    changes made to 1500efe5a2335683e2d7cec485617f5a3182776d.

--- frontend/src/core/application/ports/ProfesorRepository.ts
+++ frontend/src/core/application/ports/ProfesorRepository.ts
@@ -2,6 +2,4 @@ import type { Profesor } from '../../domain/Profesor'
 
 export interface ProfesorRepository {
   getProfesores: () => Promise<Profesor[]>
-  crearProfesor: (datos: Profesor) => Promise<Profesor>
-  actualizarStatus: (cedula: string, status: Profesor['status']) => Promise<Profesor>
 }
